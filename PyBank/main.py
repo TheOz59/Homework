a39c6774b1b1677f878bbd0b5c7bca79131c72ec
Jonathan Orozco <jonathanorozco@Jonathans-MacBook.local>
2017-11-12 19:09:24 -0800

    Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,32 @@
 import os
 import csv
 
-f1 = os.path.join("Resources, budget_data_1.csv")
-f2 = os.path.join("Resources, budget_data_1.csv")
+f1 = os.path.join("budget_data_1.csv")
+f2 = os.path.join("budget_data_1.csv")
 
-csv1 = csv.reader(f1)
-csv2 = csv.reader(f2)
+budget_data = {}
 
- 
+
+with open(f1, newline ="") as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=",")   
+	new_line = next(csvreader)
+	
+	
+	for x in csvreader:
+		row = x
+		row[1] = float(x[1])
+		
+		if row[0] in budget_data:
+			budget_data[row[0]] += row[1]
+		else:
+			budget_data[row[0]] = row[1]
+
+
+	print(budget_data)
+	
+	
+
+
+			
+
+
